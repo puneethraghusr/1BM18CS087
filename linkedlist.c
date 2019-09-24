6edd5b76c97965ef9687dbf24ad564a51da0de28
puneethraghusr <53910050+puneethraghusr@users.noreply.github.com>
2019-09-24 12:16:40 +0530

    Update linkedlist.c

--- linkedlist.c
+++ linkedlist.c
@@ -1,80 +1,130 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-typedef struct node
-{int data;
-struct node *next;
-}node;
+typedef struct List
+{
+	int data;
+	struct List *next;
+}list;
 
-node*temp;
-node*head=0;
+list *temp;
+list *head=0;
 
 void insert_at_front(int item)
 {
-
-temp=(node*) malloc (sizeof (node));
-temp->data=item;
-temp->next=head;
-
-head=temp;
+	
+	temp=(list*)malloc(sizeof(list));
+	temp->data=item;
+	temp->next=head;
+	head=temp;
+	
 }
 
-void insert_at_end(int item)
-{node *ptr;
-ptr=head;
-temp=(node*) malloc (sizeof(node));
-temp->data=item;
+void insert_at_rear(int item)
+{
+	list *prt;
+	prt=head;
+	temp=(list*)malloc(sizeof(list));
+	temp->data=item;
+	if(head==0)
+	{
+		temp->next=head;
+		head=temp;
+	}
+	else
+	{	
+		while(prt->next!=0)
+		{
+			prt=prt->next;
+		}
+		prt->next=temp;
+	}
+}
 
-if(head==0)
-{temp->next=head;
-head=temp;
+void insert_at_between(int item,int pos,int count)
+{	
+	int i;
+	if(pos>count)
+	{
+		printf("\n invalid input");
+		return;
+	}
+	else
+	{
+		list *prt;
+		prt=head;
+		temp=(list*)malloc(sizeof(list));
+		temp->data=item;	
+		if(pos==1)
+		{
+			temp->next=head;
+			head=temp;
+		}
+		else
+		{
+			for(i=0;i<pos-1;i++)
+			{
+				prt=prt->next;
+			}
+			temp->next=prt->next;
+			prt->next=temp;
+		}
+	}
 }
-else
-{while(ptr->next!=0)
-{ptr=ptr->next;
-}
-ptr->next=temp;
 
-}
-}
 void display()
-{temp=head;
-if(temp==0)
-{printf("\n lst is empty");
-return;
+{
+	temp=head;
+	if(temp==0)
+	{
+		printf("\nlist is empty");
+		return;
+	}
+	else
+	{
+		while(temp!=0)
+		{
+			printf("\n%d",temp->data);
+			temp=temp->next;
+		}
+	}
 }
-else
-{while(temp!=0)
-{printf("\n %d",temp->data);
-temp=temp->next;
-}
-}
-}
+
 int main()
 {
-
-int item,n;
+	
+	int item,n,pos,count=0;
 	while(1)
 	{
-	printf("choose an operation");
-	printf("\n1.insert at front \n2.insert at end\n3. display\n4.exit\n choice:");
-	scanf("%d",&n);
-	if(n==1||n==2)
-	{	printf("\n enter an element:");
-		scanf("%d",&item);
-	}
-switch(n)
-{
+		printf("\n\nchoose an operation :");
+		printf("\n1.insert at front\n2.insert at rear\n3.insert at between\n4.display\n5.exit\n\nchoice :");
+		scanf("%d",&n);
+		if(n==1 || n==2 || n==3)
+		{
+			printf("\nenter an element :");
+			scanf("%d",&item);
+			count++;
+		}
+		switch(n)
+		{
+			case 1: insert_at_front(item);
+				break;
+		
+			case 2: insert_at_rear(item);
+				break;
 
-case 1:insert_at_front(item);
-break;
-case 2: insert_at_end(item);
-break;
-case 3: display();
-break;
-case 4:exit(0);
-default:printf("invalid input");
-}
-}
-return 0;
+			case 3:printf("\n\nenter position :");
+				scanf("%d",&pos);
+				insert_at_between(item,pos,count);
+				break;
+
+			case 4: display();
+				break;
+			
+			case 5: exit(0);
+			
+			default: printf("\nINVALID INPUT");
+		}
+	}
+	return 0;
 }
