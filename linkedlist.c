75eb90b0c9feed67f267ebcf01b304eb2a1f4e8e
puneethraghusr <53910050+puneethraghusr@users.noreply.github.com>
2019-09-24 11:33:02 +0530

    Update linkedlist.c

--- linkedlist.c
+++ linkedlist.c
@@ -1,81 +1,80 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-typedef struct node()
-{
-int data;
-struct node*next;
+typedef struct node
+{int data;
+struct node *next;
 }node;
+
 node*temp;
+node*head=0;
 
 void insert_at_front(int item)
 {
-temp=(node*)malloc(sizeof (node))
-temp->data=item;
-temp->next=*head;
 
-*head=temp;
+temp=(node*) malloc (sizeof (node));
+temp->data=item;
+temp->next=head;
+
+head=temp;
 }
 
 void insert_at_end(int item)
-{
-node*ptr;
+{node *ptr;
 ptr=head;
-temp=(node*)malloc(sizeof node));
+temp=(node*) malloc (sizeof(node));
 temp->data=item;
 
 if(head==0)
-{
-temp->next=head;
+{temp->next=head;
 head=temp;
 }
 else
-{
-while(ptr->next!=0)
+{while(ptr->next!=0)
 {ptr=ptr->next;
 }
+ptr->next=temp;
+
 }
 }
-void display(node*temp)
-{
-temp=head;
+void display()
+{temp=head;
 if(temp==0)
-printf("\n List is empty");
+{printf("\n lst is empty");
 return;
+}
 else
-{
-while(temp!=0)
-{
-printf("\n %d",temp->data);
+{while(temp!=0)
+{printf("\n %d",temp->data);
 temp=temp->next;
+}
 }
 }
 int main()
 {
-node*head=null;
-node*ptr=null;
+
 int item,n;
-while(1)
-{
-printf("choose an operation");
-printf("\n 1)Insert at front \n 2)Insert at end \n 3)Display \n 4)Exit \n choice:");
-scanf("%d",&n);
+	while(1)
+	{
+	printf("choose an operation");
+	printf("\n1.insert at front \n2.insert at end\n3. display\n4.exit\n choice:");
+	scanf("%d",&n);
+	if(n==1||n==2)
+	{	printf("\n enter an element:");
+		scanf("%d",&item);
+	}
 switch(n)
 {
-case 1: printf("Enter an element");
-	scanf("%d",&item);
-	insert_at_front(item,*head);
-	break;
-case 2: printf("Enter an element");
-	scanf("%d",&item);
-	insert_at_end(item,head,ptr);
-	break;
-case 3: exit(0);
-default: printf("Invalid input");
+
+case 1:insert_at_front(item);
+break;
+case 2: insert_at_end(item);
+break;
+case 3: display();
+break;
+case 4:exit(0);
+default:printf("invalid input");
 }
 }
 return 0;
 }
-
-
-
